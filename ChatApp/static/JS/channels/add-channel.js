9e7a031c27a302c660d45b852ea6c4af1087e04f
Hallesite <antares.galaxy.0358@gmail.com>
2023-11-02 15:44:18 +0900

    Frontend-9999

---
+++ ChatApp/static/JS/channels/add-channel.js
@@ -0,0 +1,32 @@
+const addChannelModal = document.getElementById("add-channel-modal");
+const addPageButtonClose = document.getElementById("add-page-close-button");
+const addChannelConfirmButton = document.getElementById("add-channel-confirmation-button");
+
+// チャンネルを登録する時の処理
+
+const addChannelModal = document.getElementById("add-channel-modal");
+const addPageButtonClose = document.getElementById("add-page-close-button");
+const addChannelConfirmButton = document.getElementById("add-channel-confirmation-button");
+
+// pagination.jsでチャンネル一覧が表示されるのを待ってから読みこまれる
+// (チャンネル一覧を表示する処理が終わるまでaddChannnelButtonは存在しないためundefinedになる)
+const loadAddChannelButton = () => {
+  const addChannelButton = document.getElementById("add-channel-button");
+
+  // モーダル表示ボタンが押された時にモーダルを表示する
+  addChannelButton.addEventListener("click", () => {
+    addChannelModal.style.display = "flex";
+  });
+};
+
+// モーダル内のXボタンが押された時にモーダルを非表示にする
+addPageButtonClose.addEventListener("click", () => {
+  addChannelModal.style.display = "none";
+});
+
+// 画面のどこかが押された時にモーダルを非表示にする
+addEventListener("click", (e) => {
+  if (e.target == addChannelModal) {
+    addChannelModal.style.display = "none";
+  }
+});
