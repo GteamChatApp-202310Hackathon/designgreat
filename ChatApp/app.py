b06138421ec57f3e52ccfeb5e25584eca0f9acec
case5641git <t.hiroto.job@gmail.com>
2023-11-02 22:33:04 +0900

    feature/Backend-020

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,8 +1,116 @@
-from flask import Flask
-from routes.user_routes import user_routes
+from flask import Flask, request, redirect, render_template, session, flash, abort
+from datetime import timedelta
+import hashlib
+import uuid
+import re
+
+from models import dbConnect
 
 app = Flask(__name__)
-app.register_blueprint(user_routes, url_prefix='/')
+app.secret_key = uuid.uuid4().hex
+app.permanent_session_lifetime = timedelta(days=30)
+
+#Constant definition
+EMAIL_PATTERN = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+TEACHER_PASSWORD = "secret_teacher_password"  # Temporary teacher password
+
+#Display login page
+@app.route('/login')
+def signup():
+  return render_template('hoge')
+
+#Hashing passwords whith SHA-256
+def hash_password(password):
+  return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+#Process login
+@app.route('/login', methods=['POST'])
+def user_login():
+  email = request.form.get('email')
+  password = request.form.get('password')
+
+  if not email or not password:
+    flash('空のフォームがあるようです')
+    return redirect('/login')
+  
+  user = dbConnect.getUser(email)
+  hashed_password = hash_password(password)
+
+  if user is None or hashed_password != user["password"]:
+    flash('メールアドレスまたはパスワードが間違っています')
+    return redirect('/login')
+  
+  session['user_id'] = user["user_id"]
+  return redirect('/')
+
+#Logout
+@app.route('/login')
+def logout():
+  session.clear()
+  return redirect('/login')
+
+#Display Signup Page
+@app.route('/signup')
+def signup():
+  return render_template('registration/signup.html')
+
+#Validating signup input
+def validate_signup_input(name, email, password1, password2, teacher_password, is_teacher):
+  errors = {}
+  if not all([name, email, password1, password2]):
+    errors['empty'] = '入力されていない項目があります'
+  if password1 != password2:
+    errors['password_missmatch'] = 'パスワードが一致しません'
+  if re.match(EMAIL_PATTERN, email) is None:
+    errors['email_injustice'] = '正しいメールアドレスの形式ではありません'
+  if dbConnect.getUser(email) is not None:
+    errors['email_exist'] = '既に登録されたメールアドレスです'
+  if is_teacher and teacher_password != TEACHER_PASSWORD:
+    errors['teacher_injustice'] = ('不正な教員パスワードです')
+  return errors
+
+#Process signup
+@app.route('/signup', methods=['POST'])
+def user_signup():
+  name = request.form.get('name')
+  email = request.form.get('email')
+  password1 = request.form.get('password1')
+  password2 = request.form.get('password2')
+  teacher_password = request.form.get('teacher_password')
+  is_teacher = 'teacher' in request.form #教員チェックボックスがチャックされているか
+
+  error_messages = validate_signup_input(name, email, password1, password2, teacher_password, is_teacher)
+  if error_messages:
+    return render_template('signup.html', error_messages=error_messages)
+  
+  user_id = str(uuid.uuid4())
+  hashed_password = hashlib.sha256(password1.encode('utf-8')).hexdigest()
+  dbConnect.createUser(user_id, name, email, hashed_password)
+  session['user_id'] = user_id
+  return redirect('/')
+
+# チャンネル一覧ページの表示
+@app.route('/')
+def index():
+    uid = session.get("uid")
+    if uid is None:
+        return redirect('/login')
+    else:
+        channels = dbConnect.getChannelAll()
+        channels.reverse()
+        user_role = dbConnect.getUserRole(uid)  # ユーザーの役割を取得
+    return render_template('index.html', channels=channels, uid=uid, role=user_role)
+
+
+#Display 404 error page
+@app.errorhandler(404)
+def show_error404(error):
+    return render_template('error/404.html'),404
+
+#Display 500 error page
+@app.errorhandler(500)
+def show_error500(error):
+    return render_template('error/500.html'),500
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", debug=True) #開発中はTrue
+  app.run(host="0.0.0.0", debug=False)
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -1,1 +1,63 @@
-from models import UserDB
+import pymysql
+from util.DB import DB
+from flask import abort
+
+class dbConnect:
+  #Get user info at login.
+  def getUser(email):
+    try:
+      dbconn = DB.getConnection()
+      cur = dbconn.cursor()
+      sql = "SELECT * FROM users WHERE email=%s"
+      cur.execute(sql, (email))
+      user = cur.fetchone()
+      return user
+    except Exception as e:
+      print(e + 'が発生しています')
+      abort(500)
+    finally:
+      cur.close()
+
+  #Create user at signin
+  def createUser(user_id, name, email, password, teacher_password):
+    try:
+      conn = DB.getConnection()
+      cur = conn.cursor()
+      sql = "INSERT INTO users(id, user_name, password, teacher_password, email, role_id) VALUES(%s, %s, %s, %s, %s);"
+      cur.execute(sql, (user_id, name, email, password, teacher_password))
+      conn.commit()
+    except Exception as e:
+      print(e + 'が発生しています')
+      abort(500)
+    finally:
+      cur.close()
+
+  #Get all Channel info.
+  def getChannelAll():
+    try:
+        conn = DB.getConnection()
+        cur = conn.cursor()
+        sql = "SELECT * FROM channels;"
+        cur.execute(sql)
+        channels = cur.fetchall()
+        return channels
+    except Exception as e:
+        print(str(e) + 'が発生しています')
+        abort(500)
+    finally:
+        cur.close()
+
+  # ユーザーの役割を取得する関数
+  def getUserRole(uid):
+    try:
+        conn = DB.getConnection()
+        cur = conn.cursor()
+        sql = "SELECT role_name FROM users INNER JOIN roles ON users.role_id = roles.id WHERE users.id = %s;"
+        cur.execute(sql, (uid,))
+        result = cur.fetchone()
+        return result['role_name']
+    except Exception as e:
+        print(str(e) + 'が発生しています')
+        abort(500)
+    finally:
+        cur.close()
