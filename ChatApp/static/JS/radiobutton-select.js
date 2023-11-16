1366ba516afbef752e1f307c1c0ccdb4185e24d8
Toshiki Matuura <t.matsuura1992@gmail.com>
2023-11-16 15:11:35 +0900

    feature/Frontend-173-2

--- ChatApp/static/JS/radiobutton-select.js
+++ ChatApp/static/JS/radiobutton-select.js
@@ -1,13 +1,20 @@
-var selecterBox = document.getElementById('visibility');
-let count = 0;
-
+const flash = document.getElementById('visibility-flash');
+if (flash != null){
     function buttonClick() {
-        count += 1;
-        if (count % 2 == 0 ) {
-            selecterBox.style.visibility = "visible";
-        }        else {
-            selecterBox.style.visibility = "hidden";
-        }
+        ;
     }
-    window.addEventListener('load', buttonClick());
+}else{
+    const selecterBox = document.getElementById('visibility');
+    let count = 0;
+
+        function buttonClick() {
+            count += 1;
+            if (count % 2 == 0 ) {
+                selecterBox.style.visibility = "visible";
+            }        else {
+                selecterBox.style.visibility = "hidden";
+            }
+        }
+        window.addEventListener('load', buttonClick());
+}
 
